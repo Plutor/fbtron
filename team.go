1929be90b462e8f6905e1f753d41e162ca14fe19
Logan Ingalls <log@plutor.org>
2014-03-30 21:15:09 -0400

    Add players to a team

--- team.go
+++ team.go
@@ -1,3 +1,21 @@
 package fbtron
 
-// TODO
+type Team struct {
+  name    string
+  roster  []*Player
+  wins    int
+}
+
+func (team *Team) AddPlayer(p *Player) {
+  nthplayer := len(team.roster) + 1
+  if nthplayer > cap(team.roster) {
+    newroster := make([]*Player, nthplayer*2)
+    copy(team.roster, newroster)
+    team.roster = newroster
+  }
+
+  team.roster = team.roster[0:nthplayer]
+  team.roster[nthplayer-1] = p
+}
+
+//func (team *Team) Get
--- team_test.go
+++ team_test.go
@@ -4,6 +4,19 @@ import (
   "testing"
 )
 
-func TestTeamUnimplemented(t *testing.T) {
-  t.Log("This function will be removed when tests are implemented")
+// TODO: Separate roster into actually drafted and simulated draft
+
+func TestTeamAddPlayer(t *testing.T) {
+  team := new(Team)
+  for n := 1; n < 99; n++ {
+    player := new(Player)
+    team.AddPlayer(player)
+    if v := len(team.roster); v != n {
+      t.Errorf("Error adding player to a team: expected %d, got %d", n, v)
+    }
+  }
 }
+
+// TODO: Release (releases simulated only)
+
+// TODO: Get team-wide stats
