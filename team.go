1596b88ec188043003d896d3ee1fc11108fa6645
Logan Ingalls <log@plutor.org>
2014-03-31 08:35:48 -0400

    Keepers, Release(), bug fixes

--- team.go
+++ team.go
@@ -1,21 +1,58 @@
 package fbtron
 
 type Team struct {
-  name    string
-  roster  []*Player
-  wins    int
+  name        string
+  roster      []*Player
+  keeper      []bool
+  wins        int
 }
 
-func (team *Team) AddPlayer(p *Player) {
+// AddPlayer adds the passed player to the team roster. If keeper is true, the
+// player will not be released by the Release() call.
+func (team *Team) AddPlayer(p *Player, keeper bool) {
+  // Grow the arrays if necessary. Always assume that they're the same length.
   nthplayer := len(team.roster) + 1
   if nthplayer > cap(team.roster) {
-    newroster := make([]*Player, nthplayer*2)
-    copy(team.roster, newroster)
+    newroster := make([]*Player, len(team.roster), nthplayer * 2)
+    copy(newroster, team.roster)
     team.roster = newroster
+
+    newkeeper := make([]bool, len(team.roster), nthplayer * 2)
+    copy(newkeeper, team.keeper)
+    team.keeper = newkeeper
   }
 
+  // Add to the end of the arrays
   team.roster = team.roster[0:nthplayer]
   team.roster[nthplayer-1] = p
+  team.keeper = team.keeper[0:nthplayer]
+  team.keeper[nthplayer-1] = keeper
+}
+
+// Release releases all of the players in the roster that are not marked as
+// keepers. Returns an array of the released players.
+func (team *Team) Release() []*Player {
+  rostersize := len(team.roster)
+  newroster := make([]*Player, 0, rostersize)
+  newkeeper := make([]bool, 0, rostersize)
+  released := make([]*Player, 0, rostersize)
+
+  for i := 0; i < rostersize; i++ {
+    if team.keeper[i] {
+      newroster = newroster[:len(newroster)+1]
+      newroster[len(newroster)-1] = team.roster[i]
+      newkeeper = newkeeper[:len(newkeeper)+1]
+      newkeeper[len(newkeeper)-1] = true
+    } else {
+      released = released[:len(released)+1]
+      released[len(released)-1] = team.roster[i]
+    }
+  }
+
+  team.roster = newroster
+  team.keeper = newkeeper
+
+  return released
 }
 
 //func (team *Team) Get
--- team_test.go
+++ team_test.go
@@ -1,22 +1,51 @@
 package fbtron
 
 import (
+  "strconv"
   "testing"
 )
 
-// TODO: Separate roster into actually drafted and simulated draft
-
 func TestTeamAddPlayer(t *testing.T) {
   team := new(Team)
-  for n := 1; n < 99; n++ {
+  for n := 0; n < 99; n++ {
     player := new(Player)
-    team.AddPlayer(player)
-    if v := len(team.roster); v != n {
-      t.Errorf("Error adding player to a team: expected %d, got %d", n, v)
+    player.name = strconv.Itoa(n)
+    team.AddPlayer(player, true)
+    if v := len(team.roster); v != n + 1 {
+      t.Errorf("Error adding player to a team: expected %d, got %d", n + 1, v)
+      t.FailNow()
+    }
+  }
+
+  for n := 0; n < 99; n++ {
+    if team.roster[n].name != strconv.Itoa(n) {
+      t.Errorf("Error adding player to a team: player %d " +
+               "expected name '%s', got '%s'",
+               n, strconv.Itoa(n), team.roster[n].name )
+      t.FailNow()
     }
   }
 }
 
-// TODO: Release (releases simulated only)
+func TestRelease(t *testing.T) {
+  team := new(Team)
+  for n := 0; n < 99; n++ {
+    player := new(Player)
+    team.AddPlayer(player, n < 50)
+    if v := len(team.roster); v != n + 1 {
+      t.Errorf("Error adding player to a team: expected %d, got %d", n + 1, v)
+    }
+  }
+
+  released := team.Release()
+  if len(released) != 49 {
+    t.Errorf("Error releasing non-keeper players: " +
+             "expected 49 released, got %d", len(released))
+  }
+  if v := len(team.roster); v != 50 {
+    t.Errorf("Error releasing non-keeper players: " +
+             "expected 50 remaining, got %d", v)
+  }
+}
 
 // TODO: Get team-wide stats
