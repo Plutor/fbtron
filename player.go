085fb9304433c89b7aa27be6bf733ef9004e9a69
Logan Ingalls <log@plutor.org>
2014-03-30 20:55:18 -0400

    Player is pretty much just a struct

--- player.go
+++ player.go
@@ -1,3 +1,21 @@
 package fbtron
 
-// TODO: Data holder
+type Player struct {
+  name string
+  stats map[string]float64
+}
+
+func (p *Player) SetStat(name string, value float64) {
+  if p.stats == nil {
+    p.stats = make(map[string]float64)
+  }
+  p.stats[name] = value
+}
+
+func (p *Player) GetStat(name string) float64 {
+  val, ok := p.stats[name]
+  if ok {
+    return val
+  }
+  return -1.0
+}
--- player_test.go
+++ player_test.go
@@ -7,3 +7,11 @@ import (
 func TestPlayerUnimplemented(t *testing.T) {
   t.Log("This function will be removed when tests are implemented")
 }
+
+func TestSetStat(t *testing.T) {
+  p := new(Player)
+  p.SetStat("test", 1.0)
+  if v:= p.GetStat("test"); v != 1.0 {
+    t.Errorf("Failure to set/get a stat: expected 1.0, got %f", v)
+  }
+}
