8e453288dad1f11c26b315a9748d711b44c62719
Logan Ingalls <log@plutor.org>
2014-04-03 15:51:30 -0400

    --threads and --cpus flags

--- main.go
+++ main.go
@@ -3,20 +3,31 @@ package main
 import (
   "fbtron/fbtron"
 
+  "flag"
   "fmt"
   "runtime"
   "time"
 )
 
+var num_threads = flag.Int(
+    "threads", 0, "Number of threads to start (0 = number of cpus)")
+var num_cpus = flag.Int(
+    "cpus", 0, "Number of CPUs to use (0 = all on system)")
+
 func main() {
-  fmt.Println("Starting")
-
   // Tell Go to use all of the processors
-  num_cpus := runtime.NumCPU()
-  runtime.GOMAXPROCS(num_cpus)
+  if *num_cpus == 0 {
+    *num_cpus = runtime.NumCPU()
+  }
+  runtime.GOMAXPROCS(*num_cpus)
+  if *num_threads == 0 {
+    *num_threads = *num_cpus
+  }
 
-  sendchannels := make([]chan string, num_cpus)
-  recvchannels := make([]chan fbtron.Simulation, num_cpus)
+  fmt.Printf("Starting %d threads on %d cpus\n", *num_threads, *num_cpus)
+
+  sendchannels := make([]chan string, *num_threads)
+  recvchannels := make([]chan fbtron.Simulation, *num_threads)
   for n := range sendchannels {
     sendchannels[n] = make(chan string, 1)
     recvchannels[n] = make(chan fbtron.Simulation, 1)
